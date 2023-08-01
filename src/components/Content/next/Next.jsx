2196eed1bc205cddaf7861d41099734d193b5973
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-08-01 21:09:51 +0530

    Update Next.jsx

--- src/components/Content/next/Next.jsx
+++ src/components/Content/next/Next.jsx
@@ -5,8 +5,8 @@ import { NavLink } from 'react-router-dom';
 
 const Next = ({ hasPreviousPage, hasNextPage }) => (
   <>
-    <div className="fixed z-0 flex h-full w-full items-center justify-between p-2 dark:bg-slate-800 dark:text-white md:p-5 lg:p-1">
-      <div className="z-0 flex w-full justify-between">
+    <div className="fixed z-0 flex h-full w-full items-center justify-center p-2 dark:bg-slate-800 dark:text-white md:p-5 lg:p-1">
+      <div className="z-0 flex w-full items-center justify-between">
         {hasPreviousPage && (
           <NavLink to="/home">
             <button type="button" className=" text-pink-600 dark:text-white">
