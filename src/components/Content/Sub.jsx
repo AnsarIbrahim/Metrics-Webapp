87bc60c832ce3a7bec2b6f793780857260288a85
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-08-01 21:23:08 +0530

    Update Sub.jsx

--- src/components/Content/Sub.jsx
+++ src/components/Content/Sub.jsx
@@ -53,7 +53,7 @@ const SubMain = () => {
       ) : (
         <>
           <Next hasPreviousPage={hasPreviousPage} hasNextPage={hasNextPage} />
-          <div className="flex h-full w-[full] flex-col items-center justify-center overflow-y-auto scroll-smooth bg-pink-800 px-5 pb-20 pt-64 hover:scroll-auto dark:bg-slate-800 dark:text-white sm:pt-[27%] md:pt-44">
+          <div className="flex h-full w-[full] flex-col items-center justify-center overflow-y-auto scroll-smooth bg-pink-800 px-8 pb-20 pt-64 hover:scroll-auto dark:bg-slate-800 dark:text-white sm:pt-[27%] md:pt-44">
             <div className="z-30 grid h-full w-[70wh] grid-cols-2 gap-4 p-3 shadow shadow-slate-200 dark:bg-slate-800 dark:text-white sm:mt-0 sm:w-[70wh] sm:grid-cols-3 md:w-[70wh] md:grid-cols-3 lg:w-[80wh] lg:grid-cols-4 xl:w-[70wh] xl:grid-cols-6">
               {fetchedKurals.map((kural, index) => (
                 <div
